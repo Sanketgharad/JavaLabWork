a07be01db9ec000672c571f6da498d15f3adec73
Sanketgharad <155932198+Sanketgharad@users.noreply.github.com>
2024-02-25 13:57:48 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,50 @@
+import java.lang.Math;
+public class Calculator {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public long add(int a, int b) {
+
+        return a + b;
+
+    }
+
+    public int sub(int a, int b) {
+
+        return a - b;
+    }
+
+    public int multiplication(int a, int b) {
+
+        return a * b;
+    }
+
+    public int division(int a, int b) {
+
+        return a / b;
+    }
+
+    public long square_a(int a) {
+
+        return a*a;
+    }
+
+    public double square_root(int a) {
+
+        return Math.sqrt(a);
+    }
+
+    public int modulo(int a, int b) {
+
+        return a % b;
+    }
+
+    public double log(int a) {
+
+        return Math.log(a);
+    }
+}
